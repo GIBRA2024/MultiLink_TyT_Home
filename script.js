afc4adf701b22c779e773d82a4b8d04c84d5e781
Mateo <monorymateo1@gmail.com>
2024-12-19 16:08:04 -0500

    inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+function descargarContacto() {
+    const vcardData = `BEGIN:VCARD
+VERSION:3.0
+FN:Paula Jaramillo
+TEL:+57 3052500042
+EMAIL:ventas@guajiraamano.com
+NOTE: Guajira a Mano
+END:VCARD`;
+
+    const blob = new Blob([vcardData], { type: 'text/vcard' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'contacto.vcf';
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+}
